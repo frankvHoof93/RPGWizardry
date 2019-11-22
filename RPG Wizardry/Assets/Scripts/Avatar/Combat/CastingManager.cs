0868e10ec966213f2bfddee947ae987ce81baabc
Emily Horvers <thorvers@xs4all.nl>
2019-11-22 13:14:34 +0100

    Castingmanager cleanup, comments

--- RPG Wizardry/Assets/Scripts/Avatar/Combat/CastingManager.cs
+++ RPG Wizardry/Assets/Scripts/Avatar/Combat/CastingManager.cs
@@ -8,30 +8,79 @@ namespace nl.SWEG.RPGWizardry.Avatar.Combat
     [RequireComponent(typeof(InputState))]
     public class CastingManager : MonoBehaviour
     {
+        #region Variables
+        #region Public
+        /// <summary>
+        /// Prototype projectile; fill this with selected spell later
+        /// </summary>
+        public Projectile projectilePrefab;
+        #endregion
+        #region Private
+        /// <summary>
+        /// Transform of the object the projectiles need to spawn from
+        /// </summary>
         [SerializeField]
         private Transform spawnLocation;
-        public GameObject projectilePrefab;
+        /// <summary>
+        /// Inputstate for getting button states
+        /// </summary>
         private InputState inputState;
+        /// <summary>
+        /// Cooldown state during which you cannot cast spells
+        /// </summary>
+        private bool cooldown = false;
+        #endregion
+        #endregion
 
-        // Start is called before the first frame update
+        #region Methods
+        #region Unity
+        /// <summary>
+        /// Grabs inputstate reference for button presses
+        /// </summary>
         void Start()
         {
             inputState = GetComponent<InputState>();
         }
 
-        // Update is called once per frame
+        /// <summary>
+        /// If the button is pressed and there's no cooldown, fire the spell
+        /// </summary>
         void Update()
         {
             if (inputState.Cast1)
             {
-                SpawnProjectile(projectilePrefab);
+                if (!cooldown)
+                {
+                    SpawnProjectile(projectilePrefab);
+                }
             }
         }
+        #endregion
+        #region Private
 
-        void SpawnProjectile(GameObject projectile)
+        /// <summary>
+        /// Spawns a projectile, then puts the casting system on cooldown based on the spell
+        /// </summary>
+        /// <param name="projectile">Projectile of the spell to cast; contains cooldown value</param>
+        void SpawnProjectile(Projectile projectile)
         {
-            Instantiate(projectile, spawnLocation.position, spawnLocation.rotation);
+            Instantiate(projectile.gameObject, spawnLocation.position, spawnLocation.rotation);
+            StartCoroutine(Cooldown(projectile.Cooldown));
         }
-    }
 
+        /// <summary>
+        /// Cooldown state during which no spells may be cast
+        /// </summary>
+        /// <param name="coolSeconds">Seconds the cooldown should remain active</param>
+        /// <returns></returns>
+        IEnumerator Cooldown(int coolSeconds)
+        {
+            cooldown = true;
+            yield return new WaitForSeconds(coolSeconds);
+            cooldown = false;
+
+        }
+        #endregion
+        #endregion
+    }
 }
--- RPG Wizardry/Assets/Scripts/Gameplay/Projectile.cs
+++ RPG Wizardry/Assets/Scripts/Gameplay/Projectile.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Projectile : MonoBehaviour {
     
     private int movementSpeed = 2;
+    public int Cooldown = 1;
 
     // Update is called once per frame
     void FixedUpdate()
