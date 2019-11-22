a4d730c99322d5f7ca30d9dd04380d1f11919161
Emily Horvers <thorvers@xs4all.nl>
2019-11-22 11:45:19 +0100

    Scriptable enemies, spells; enums for elements and patterns

---
+++ RPG Wizardry/Assets/EnemyData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyData", menuName = "ScriptableObjects/EnemyData", order = 1)]
+public class EnemyData : ScriptableObject
+{
+    public string enemyName;
+    public int enemyHealth;
+    public int enemyAttack;
+    public float enemySpeed;
+}
---
+++ RPG Wizardry/Assets/SpellData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using nl.SWEG.RPGWizardry.Sorcery;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SpellData", menuName = "ScriptableObjects/SpellData", order = 1)]
+public class SpellData : ScriptableObject
+{
+    public string spellName;
+    public SpellPattern spellPattern;
+    public int spellRange;
+    public int spellDamage;
+    public Element spellElement;
+    public int spellCooldown;
+}
