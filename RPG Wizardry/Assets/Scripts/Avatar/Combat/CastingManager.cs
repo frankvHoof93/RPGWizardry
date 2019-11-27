d75d60a6e9d674ef5a38c24f3e614e25e0d4bd9f
Emily Horvers <thorvers@xs4all.nl>
2019-11-27 15:04:57 +0100

    Book closes when shooting projectile

--- RPG Wizardry/Assets/Scripts/Avatar/Combat/CastingManager.cs
+++ RPG Wizardry/Assets/Scripts/Avatar/Combat/CastingManager.cs
@@ -29,6 +29,12 @@ namespace nl.SWEG.RPGWizardry.Avatar.Combat
         /// Cooldown state during which you cannot cast spells
         /// </summary>
         private bool cooldown = false;
+
+        /// <summary>
+        /// Animator for the book
+        /// </summary>
+        [SerializeField]
+        private Animator bookAnimator;
         #endregion
         #endregion
 
@@ -64,6 +70,9 @@ namespace nl.SWEG.RPGWizardry.Avatar.Combat
         /// <param name="projectile">Projectile of the spell to cast; contains cooldown value</param>
         void SpawnProjectile(Projectile projectile)
         {
+            //start animation
+            bookAnimator.SetBool("Cast", true);
+            
             //spawn projectile at book's location, book's rotation
             Instantiate(projectile.gameObject, spawnLocation.position, spawnLocation.rotation);
             //start cooldown
@@ -78,7 +87,10 @@ namespace nl.SWEG.RPGWizardry.Avatar.Combat
         /// <returns></returns>
         IEnumerator Cooldown(float coolSeconds)
         {
-            yield return new WaitForSeconds(coolSeconds);
+            yield return new WaitForSeconds(0.1f);
+            //turn off animation so it only plays once
+            bookAnimator.SetBool("Cast", false);
+            yield return new WaitForSeconds(coolSeconds - 0.1f);
             cooldown = false;
 
         }
