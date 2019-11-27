0584c7fc867cdb65623e1c58b91e6950c3ecc823
Frank van Hoof <frankvhoof.dtt@gmail.com>
2019-11-27 13:17:30 +0100

    Updated Script-Positions & NameSpaces

---
+++ RPG Wizardry/Assets/Scripts/Entities/Enemies/EnemyData.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace nl.SWEG.RPGWizardry.Entities.Enemies
+{
+    [CreateAssetMenu(fileName = "EnemyData", menuName = "ScriptableObjects/EnemyData", order = 1)]
+    public class EnemyData : ScriptableObject
+    {
+        public string Name => enemyName;
+        public int Health => enemyHealth;
+        public int Attack => enemyAttack;
+        public float Speed => enemySpeed;
+
+        [SerializeField]
+        private string enemyName;
+        [SerializeField]
+        private int enemyHealth;
+        [SerializeField]
+        private int enemyAttack;
+        [SerializeField]
+        private float enemySpeed;
+    }
+}
