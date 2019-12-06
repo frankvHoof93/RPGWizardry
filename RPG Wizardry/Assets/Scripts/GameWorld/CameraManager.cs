29c06bdb1f7db27e17eab312ccb21e96f8ba76b8
Emily Horvers <thorvers@xs4all.nl>
2019-12-06 13:38:08 +0100

    Accessibility fix

--- RPG Wizardry/Assets/Scripts/GameWorld/CameraManager.cs
+++ RPG Wizardry/Assets/Scripts/GameWorld/CameraManager.cs
@@ -14,13 +14,12 @@ namespace nl.SWEG.RPGWizardry.GameWorld
         /// </summary>
         private Transform playerTransform;
 
-
         #region Methods
         /// <summary>
         /// Gets reference to player transform
         /// </summary>
         // Start is called before the first frame update
-        void Start()
+        private void Start()
         {
             if (PlayerManager.Exists)
             {
@@ -32,7 +31,7 @@ namespace nl.SWEG.RPGWizardry.GameWorld
         /// <summary>
         /// Every frame, updates current position of camera to match player
         /// </summary>
-        void Update()
+        private void Update()
         {
             transform.position = new Vector3(
                 playerTransform.position.x, playerTransform.position.y, playerTransform.position.z-10);
