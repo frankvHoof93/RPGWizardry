581c626cdaa822f3b84175f99d38f9da02fdf494
Frank van Hoof <frankvhoof.dtt@gmail.com>
2019-12-07 02:58:26 +0100

    CircleFadeDemo

--- RPG Wizardry/Assets/SeeThroughDemo.cs
+++ RPG Wizardry/Assets/SeeThroughDemo.cs
@@ -7,8 +7,8 @@ public class SeeThroughDemo : MonoBehaviour
     [SerializeField]
     private SpriteRenderer[] renderers;
     /// <summary>
-    /// MPB is a class, but is used like a struct in Unity. Thus, we can use just a single instance, as long as we read every time. 
-    /// TODO: Check timings vs memory on tis
+    /// MPB is a class, but is used like a struct in Unity. Thus, we can use just a single instance, as long as we read every time.
+    /// If this code is put on an instance (i.e. in a MonoBehaviour, you can just keep the current instance, as long as no code is trying to change properties via the renderer
     /// </summary>
     private MaterialPropertyBlock block;
 
@@ -17,9 +17,9 @@ public class SeeThroughDemo : MonoBehaviour
         block = new MaterialPropertyBlock();
         for (int i = 0; i < renderers.Length; i++)
         {
-            renderers[i].GetPropertyBlock(block);
-            block.SetFloat("_UseSeeThrough", UnityEngine.Random.Range((int)0, (int)2));
-            renderers[i].SetPropertyBlock(block);
+            renderers[i].GetPropertyBlock(block); // Read Properties
+            block.SetFloat("_UseSeeThrough", UnityEngine.Random.Range((int)0, (int)2)); // Set Float (Bool)
+            renderers[i].SetPropertyBlock(block); // Write Properties
         }
     }
 
@@ -27,12 +27,12 @@ public class SeeThroughDemo : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        Vector3 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(transform.position); // ScreenSpace-Position for Transform
         for (int i = 0; i < renderers.Length; i++)
         {
-            renderers[i].GetPropertyBlock(block);
-            block.SetVector("_SeeThroughCenter", new Vector4(screenPos.x, screenPos.y, 0, 0));
-            renderers[i].SetPropertyBlock(block);
+            renderers[i].GetPropertyBlock(block); // Read Properties
+            block.SetVector("_SeeThroughCenter", new Vector4(screenPos.x, screenPos.y, 0, 0)); // Set Position
+            renderers[i].SetPropertyBlock(block); // Write Properties
         }
     }
 }
