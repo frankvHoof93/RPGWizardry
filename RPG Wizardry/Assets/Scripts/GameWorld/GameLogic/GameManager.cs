b486ff32fe33be6a281601c0e88623a09d5e8f3f
Sven Janssen <svenjanssen12321@gmail.com>
2019-12-11 12:47:01 +0100

    Added Leantween & worked on room transitions

---
+++ RPG Wizardry/Assets/Scripts/GameWorld/GameLogic/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace nl.SWEG.RPGWizardry.GameWorld.GameLogic
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager Instance;
+
+        #region Variables
+        public GameState GameState;
+        public bool Locked;
+        #endregion
+
+        #region Methods
+        #region Unity
+        private void Start()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            Locked = false;
+        }
+        #endregion
+        #endregion
+    }
+
+    public enum GameState
+    {
+        Menu = 0,
+        GamePlay = 1
+    }
+}
---
+++ RPG Wizardry/Assets/Scripts/GameWorld/RoomSwitcher.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using nl.SWEG.RPGWizardry.Player;
+
+namespace nl.SWEG.RPGWizardry.GameWorld
+{
+    public class RoomSwitcher : MonoBehaviour
+    {
+        #region Variables     
+        //camera for fading
+
+        //transform spawnposition
+        //original room to disable?
+        //new room to activate
+        //player to move
+        //private PlayerManager
+        #endregion
+
+        #region Methods
+        #region Private
+        /// <summary>
+        /// Switches the player 
+        /// </summary>
+        private void SwitchRoom()
+        {
+            //Lock player controls or game
+
+            //Fade camera to black
+
+            //Enable new room
+
+            //Move player to new room
+
+            //Disable old room
+
+            //Unlock player controls or game
+
+            //Fade player back in
+
+            //activate enemies in new room
+
+        }
+        #endregion
+        #region Unity
+        /// <summary>
+        /// 
+        /// </summary>
+        void Start()
+        {
+            if (PlayerManager.Exists)
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// Checks if the player is hitting the room switch trigger.
+        /// </summary>
+        /// <param name="collision"></param>
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            SwitchRoom();
+        }
+        #endregion
+        #endregion
+    }
+}
